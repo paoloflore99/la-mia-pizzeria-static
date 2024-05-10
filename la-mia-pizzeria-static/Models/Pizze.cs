597e7d21e750a8ade1e7c7ed913dd551a82186dd
paolo flore <paoloflore937@gmail.com>
2024-05-10 16:34:50 +0200

    finito

---
+++ la-mia-pizzeria-static/Models/Pizze.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizze
+    {
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string UrlFoto { get; set; }
+        public double Prezzo { get; set; }
+
+        public Pizze(string nome, string descrizione, string urlfoto, double prezzo)
+        {
+            Nome = nome;
+            Descrizione = descrizione;
+            UrlFoto = urlfoto;
+            Prezzo = prezzo;
+        }
+    }
+}
